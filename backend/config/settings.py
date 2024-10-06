1436321ab5660931e5271ca596a0c5795ba483e4
rakibulislam8226 <rakibulislam8226@gmail.com>
2024-10-06 17:58:18 +0600

    default authentications class modified

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,7 +145,8 @@ CORS_ALLOWED_ORIGINS = [
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ],
 }
