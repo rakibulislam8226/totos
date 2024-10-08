ec5005210f3d759cb5b6caf11fa99cb511f9c593
rakibulislam8226 <rakibulislam8226@gmail.com>
2024-10-08 17:23:48 +0600

    authenticated user get endpoint added

--- backend/authentications/urls.py
+++ backend/authentications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserAuthView
+from .views import UserAuthView, UserMeListView
 
 urlpatterns = [
+    path("/me", UserMeListView.as_view(), name="user-me"),
     path("/register", UserAuthView.as_view(), name="auth"),
 ]
--- backend/authentications/views.py
+++ backend/authentications/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, permissions, status, views
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 
 from .serializers import UserSerializer
 
@@ -7,3 +6,11 @@ from .serializers import UserSerializer
 class UserAuthView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class UserMeListView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user if self.request.user.is_authenticated else None
